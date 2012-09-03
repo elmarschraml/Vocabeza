9f246e5b5e8ef1ed8b295e05eee4fe04a77cd24f
Elmar Schraml <elmar.schraml@gmail.com>
2012-09-03 23:19:48 +0200

    back to using datastore id, errorhandling for GET /word

--- vocabeza/api_quiz.py
+++ vocabeza/api_quiz.py
@@ -9,12 +9,12 @@ import model_word
 class QuizResource(webapp2.RequestHandler):
 
   def get(self,location):
-      logging.info("word get is not yet implemented")
+      logging.info("quiz get is not yet implemented")
       #TODO implement
 
 
   def put(self,location):
-      logging.info("word get is not yet implemented")
+      logging.info("quiz put is not yet implemented")
       #TODO implement
 
   #TODO implement log and error message when another (unsupported) operation is called
--- vocabeza/api_word.py
+++ vocabeza/api_word.py
@@ -9,17 +9,29 @@ class  WordResource(webapp2.RequestHandler):
 
 
     def get(self,location):
-      logging.info("word get is not yet implemented")
-      word_id = location.lstrip('/')
-      logging.info("looking for word with id: " + word_id)
-      q = db.Query(model_word.WordModel)
-      q.filter("wordid =",word_id)
-      found_word = q.get()
-      word_dict = db.to_dict(found_word)
-      jsontext = json.dumps(word_dict)
       self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
+      word_id = location.lstrip('/')
+      if len(word_id) == 0:
+        logging.info("GET /word: no id given, so going by request parameters for attribute search")
+        all_param_names = self.request.arguments()
+        if all_param_names == None or len(all_param_names) == 0:
+          retval = dict(code="badinput", message="Please either add an id to the url to retrieve a word, e.g. GET /word/17, or supply parameters to search, e.g. GET /word?de=beispiel")
+        else:
+          #add matching url params to query filter
+          retval = dict(message="cant handle param search yet")
+
+
+      elif not(word_id.isdigit()): #id is not numeric
+        retval = dict(code="badinput", message="word id has to be numeric")
+      else:
+        logging.info("looking for word with id: " + word_id)
+        found_word = model_word.WordModel.get_by_id(int(word_id))
+        if (found_word):
+          retval = db.to_dict(found_word)
+        else:
+          retval = dict(code="notfound", message="No word found for id " + word_id)
+      jsontext = json.dumps(retval)
       self.response.out.write(jsontext)     
-      #TODO error handling
       #TODO if no id present, go by url param for de or es
 
       
