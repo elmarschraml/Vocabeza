31b4e68649b6a757168aa172bb09d7e7aa78d136
Elmar Schraml <elmar.schraml@gmail.com>
2012-10-28 13:31:56 +0100

    implemented PUT /quiz (store result) and GET /quiz (get random word to ask

--- vocabeza/api_quiz.py
+++ vocabeza/api_quiz.py
@@ -1,23 +1,54 @@
 from google.appengine.ext import db
+from datetime import datetime
+from random import choice
 import json
 import webapp2
 import logging
+import model_quiz
 import model_word
 
 
 
 class QuizResource(webapp2.RequestHandler):
 
+  quiz_wrong_input_message = "To PUT a new quiz, you need to supply a json body, e.g. {'version': 1, 'date': '2012-10-01 10:00:00', 'answer': 'bla', 'word': 17}"     
+
   def get(self,location):
-      failtext = "quiz get is not yet implemented"
-      logging.info(failtext)
-      self.response.out.write(failtext)
-      #TODO implement
+    q = model_word.WordModel.all()
+    wordlist = q.fetch(1000)
+    chosen_word = choice(wordlist)
+    retval = db.to_dict(chosen_word)
+    jsontext = json.dumps(retval)
+    self.response.out.write(jsontext)
 
 
   def put(self,location):
-      logging.info("quiz put is not yet implemented")
-      #TODO implement
+    logging.info("read request" + self.request.body)
+    requestbody = self.request.body
+    if len(requestbody) == 0:
+      logging.info("wordlist PUT: empty request body, cannot continue")
+      retval = dict(code="badinput", message=self.quiz_wrong_input_message)
+    else:   
+        try:
+          from_request = json.loads(self.request.body)
+          logging.info("from_request: " + str(from_request))
+          new_quiz = model_quiz.QuizModel(version=1, answer='bla')
+          if "version" in from_request:
+            new_quiz.version = from_request["version"]
+          new_quiz.answer = from_request["answer"]
+          if "date" in from_request:
+            new_date = datetime.strptime(from_request["date"], "%Y-%m-%d %H:%M:%S")
+            new_quiz.date = new_date
+          logging.info("new_quiz with data: " + str(new_quiz))
+          new_quiz.put()
+          logging.info("was put in db")
+          retval = dict(code="ok", mesage="quiz for answer " + new_quiz.answer + " was saved") #TODO include whole json quiz object - needs to deal with datetime json serialization
+        except (ValueError, TypeError) as error:
+          logging.info(error)
+          retval = dict(code="badinput", message=self.quiz_wrong_input_message)
+    jsontext = json.dumps(retval)
+    self.response.out.write(jsontext)
+    
 
   #TODO implement log and error message when another (unsupported) operation is called
   def main():
--- vocabeza/model_quiz.py
+++ vocabeza/model_quiz.py
@@ -1,15 +1,15 @@
 from google.appengine.ext import db
-from model_quiz import WordModel 
+from model_word import WordModel 
 
 class QuizModel(db.Model):
-	version = db.IntegerProperty(required=True)
-	word = db.ReferenceProperty(WordModel)
-	answer = db.StringProperty(required=True)
-	date = db.DateTimeProperty(required=False)
+    version = db.IntegerProperty(required=True)
+    word = db.ReferenceProperty(WordModel)
+    answer = db.StringProperty(required=True)
+    date = db.DateTimeProperty(required=False)
 
-	
+    
     def to_dict(self):
-	       return dict([(p, unicode(getattr(self, p))) for p in self.properties()])
+      return dict([(p, unicode(getattr(self, p))) for p in self.properties()])
 
     def __str__(self):
-           return str(self.to_dict())
+      return str(self.to_dict())
